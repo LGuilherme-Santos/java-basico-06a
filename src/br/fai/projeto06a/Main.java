f809593e267677341f9f9f0ddefebf37a7ee378e
Matuk <mateusmatuk@fai-mg.br>
2020-03-09 20:02:30 -0300

    segundo commit

--- src/br/fai/projeto06a/Main.java
+++ src/br/fai/projeto06a/Main.java
@@ -10,7 +10,44 @@ public class Main {
 	}
 
 	private void start() {
-		// TODO Auto-generated method stub
+
+
+		String[] carros = {"civic", "gol", "palio", "uno"};
+		
+		System.out.println("Posição 1: " + carros[1] );
+		
+		for (String carro : carros) {
+			System.out.println("carro: " + carro);
+		}
+		
+		System.out.println("----");
+		
+		carros[1] = "L200";
+		
+		for (String carro : carros) {
+			System.out.println("carro: " + carro);
+		}
+		
+		
+		int tamanhoDoNovoArray = carros.length + 1;
+		
+		String[] novoArrayDeCarros = new String[tamanhoDoNovoArray];
+		
+		for (int i = 0; i < carros.length; i++) {
+			
+			String carro = carros[i];
+			novoArrayDeCarros[i] = carro;
+		}
+		
+		novoArrayDeCarros[tamanhoDoNovoArray - 1] = "gol";
+		
+		System.out.println("----");
+		
+		for (String carro : novoArrayDeCarros) {
+			
+			System.out.println("Carro: " + carro);
+		}
+		
 		
 	}
 
